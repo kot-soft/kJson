064c27d891f09ec78621b351766471ec6f1247c1
Константин Телеснин <kot_bcc@mail.ru>
2017-02-22 21:35:48 +0300

    Prepare package for 1.0.3

--- vs/libs/kJson.NetFramework/Properties/AssemblyInfo.cs
+++ vs/libs/kJson.NetFramework/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("959a54c6-e5a4-4a51-a2fd-fbd2f3c5273f")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
